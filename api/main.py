f7129bf349d4973670e4e707bf47bbf008c46d89
hujuu <1961946+hujuu@users.noreply.github.com>
2023-06-17 10:13:36 +0900

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from api.routers import task, done
+
+app = FastAPI()
+app.include_router(task.router)
+app.include_router(done.router)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello")
+async def hello():
+    return {"message": "hello world!"}
